72c9c48a768c40d2e41fa8d79a6c7a16af8c7651
sago <sfedeherrera@gmail.com>
2022-08-01 18:08:34 +0200

    update examples

--- pages/1_Examples.py
+++ pages/1_Examples.py
@@ -1,3 +1,43 @@
 import streamlit as st
 
-st.markdown("## Work in progress...")
+st.set_page_config(
+    page_title="Rules extraction", page_icon="📐", initial_sidebar_state="expanded",
+)
+
+hide_menu_style = """
+        <style>
+            footer {visibility: hidden;}
+            #MainMenu {visibility: hidden;}
+            ul[data-testid=main-menu-list] > ul:nth-of-type(4) > li:nth-of-type(1) {display: none;}
+            ul[data-testid=main-menu-list] > div:nth-of-type(2) {display: none;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+
+st.markdown("## Examples :pencil2:")
+
+st.markdown("#### nominal subject-verb inversion ")
+st.markdown("**P1** : pattern {e:H->X; X-[subj]->Y; Y[upos=NOUN|PROPN]}")
+st.markdown("**P2** : pattern { X << Y}")
+st.markdown("**P3** : e.label")
+st.markdown("---")
+
+st.markdown("#### noun-adjective inversion")
+st.markdown("**P1** : pattern {e:X->Y; X[upos=NOUN]; Y[upos=ADJ]}")
+st.markdown("**P2** : pattern { Y << X}")
+st.markdown("**P3** : Y.AnyFeat")
+st.markdown("---")
+
+st.markdown("#### noun number agreement")
+st.markdown("**P1** : pattern {e:X->Y; X[upos=NOUN]; X[Number=Sing|Plur]; Y[Number=Sing|Plur]}")
+st.markdown("**P2** : pattern { X.Number = Y.Number}")
+st.markdown("**P3** : e.label; X.AnyFeat; Y.AnyFeat")
+st.markdown("---")
+
+st.markdown("#### ul negation of verbs (wolof)")
+st.markdown("**P1** : pattern{ V[Polarity=Neg, upos=VERB|AUX]}; e:X->V")
+st.markdown('**P2** : pattern { V.form=re".*l" }')
+st.markdown("**P3** : e.label; V.AnyFeat; X.AnyFeat")
+st.markdown("---")
+
--- pages/2_Tutorial.py
+++ pages/2_Tutorial.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.markdown("## Work in progress...")
+st.set_page_config(
+    page_title="Rules extraction", page_icon="📐", initial_sidebar_state="expanded",
+)
+
+hide_menu_style = """
+        <style>
+            footer {visibility: hidden;}
+            #MainMenu {visibility: hidden;}
+            ul[data-testid=main-menu-list] > ul:nth-of-type(4) > li:nth-of-type(1) {display: none;}
+            ul[data-testid=main-menu-list] > div:nth-of-type(2) {display: none;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+st.markdown("## Work in progress... :construction:")
+
